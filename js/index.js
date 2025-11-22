8b66c3aaac9338d863d8b33d6f29d3f59c101a2f
salma hesham <162762766+salmahesha@users.noreply.github.com>
2025-11-22 18:07:56 +0200

    Final_project_NTI

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// var inputSearch = document.querySelector('.input-search input');
+// var overlay = document.querySelector('.overlay');
+// inputSearch.addEventListener('click' , function(){
+//     overlay.classList.add('position-absolute', 'top-0' ,'bottom-0','start-0', 'end-0','opacity-25','z- ');
+    
+// })
